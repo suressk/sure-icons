b557d91f1e7f4e0ad0d58d14cd9631d1e9dccd0d
suressk <sure_ssk@outlook.com>
2022-08-03 04:51:27 +0800

    chore: 🫧 test react icons to use

--- playground/react-pro/src/App.tsx
+++ playground/react-pro/src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-// import {} from 'sure-icons-react'
+// import React from 'react'
+import * as icons from 'sure-icons-react'
+import { getRandomHex } from 'sure-utils'
+import styles from './App.module.css'
+
+const IconsComponents = Object.entries(icons)
 
 function App() {
+  const Icons = IconsComponents.map((Icon) => {
+    const [name, Comp] = Icon
+    return (
+      <i className={styles.icon} key={name} style={{
+        color: getRandomHex()
+      }}>
+        <Comp />
+      </i>
+    )
+  })
+
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
+    <div className={styles.app}>
+      {Icons}
     </div>
   )
 }
